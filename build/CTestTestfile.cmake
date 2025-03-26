d691b61a32724719f33913b90be968eb19d1b40d
Matthewzyc <133635191+Matthewzyc@users.noreply.github.com>
2025-03-26 20:44:20 +0800

    Delete build directory

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/matthew/Workspace/path_tracker_sim/src
-# Build directory: /home/matthew/Workspace/path_tracker_sim/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("path_tracker")
--- build/path_tracker/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/matthew/Workspace/path_tracker_sim/src/path_tracker
-# Build directory: /home/matthew/Workspace/path_tracker_sim/build/path_tracker
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
